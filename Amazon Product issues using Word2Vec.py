74f0bde634359481e0ea13cadaf457501b532765
Sandeep Ramesh <SandeepRamesh@users.noreply.github.com>
2017-07-24 15:34:31 -0500

    Update Amazon Product issues using Word2Vec.py

--- Amazon Product issues using Word2Vec.py
+++ Amazon Product issues using Word2Vec.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-Created on Thu Jul 20 14:35:40 2017
+Created on Sat Jul 15 20:45:47 2017
 
 @author: sande
 '''
@@ -8,6 +8,7 @@ Created on Thu Jul 20 14:35:40 2017
 #importing libraries
 from nltk.corpus import stopwords
 import re
+import nltk
 from collections import Counter  
 import string 
 from nltk.tokenize import word_tokenize
@@ -17,6 +18,7 @@ import csv
 import requests, bs4
 import textacy
 from gensim.models import Word2Vec
+from nltk.stem import WordNetLemmatizer
 
 each_review=[]
 total_reviews=[] 
@@ -44,12 +46,13 @@ for i in range(1,13):
             file.write(line+'\n\n')
         file.close()
 
-#Reading from the saved file and preprocessing of texts
 with open('amazon.txt','r') as f:
     words=nltk.word_tokenize(f.read().lower())
        
     words1=[]
     words2=[]
+    punctuation = list(string.punctuation)
+    stops=stopwords.words("english")+ punctuation
     for w in words:
         if w not in stops and len(w) > 1:
             words1.append(w)
@@ -58,31 +61,25 @@ with open('amazon.txt','r') as f:
         if word not in addition_words:
             words2.append(word)
     
-    #Using Textacy library for easy preprocessing  
+    #tok_word=[nltk.word_tokenize(sent) for sent in words2]    
     tok_word=textacy.preprocess.preprocess_text(str(words2),no_punct=True,no_numbers=True,no_currency_symbols=True,no_accents=True)
     tok_word=re.sub('NUMBER','',tok_word)
     tok_word=nltk.word_tokenize(tok_word)
     lemmtok_words=[]
-    for word in tok_word:        
-        lemmtok_words.append(WordNetLemmatizer.lemmatize(word))
+    lemmatizer = WordNetLemmatizer()
+    for word in tok_word:
+        
+        lemmtok_words.append(lemmatizer.lemmatize(word))
     lemmtok_words_new=[nltk.word_tokenize(sent) for sent in lemmtok_words]
-    
-    #Usage of word2Vec model
     model=gensim.models.Word2Vec(lemmtok_words_new,min_count=1,size=32)
     model.most_similar('chrome')
-    lemmtok_words_new=Counter(lemmtok_words_new)
-    lemmtok_words_new.most_common(50)
-    sorted(lemmtok_words_new,key=lemmtok_words_new.get)[:50]
+    lemmtok_words_new=Counter(lemmtok_words)
+    lemmtok_words_new.most_common(30)
+    #Last 30 words
+    sorted(lemmtok_words_new,key=lemmtok_words_new.get)[:30]
     #keylist=summarize(str(total_reviews))
-    #keywords(keylist)   
-
-    #Getting the most similar words from the top 50 frequent words 
+    #keywords(keylist)        
     most_similar_words=[]
-    for i in sorted(lemmtok_words_new,key=lemmtok_words_new.get,reverse=True)[:50]:
+    for i in sorted(lemmtok_words_new,key=lemmtok_words_new.get,reverse=True)[:30]:
         if len(i) > 2:
             most_similar_words.append(model.most_similar(i))
-    
-    
-            
-            
-        
